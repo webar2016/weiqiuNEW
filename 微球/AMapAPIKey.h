a8736e4137a0fc85370de0d49dff68fd4bf14113
jiayubin <webar2016@163.com>
2016-05-10 16:16:28 +0800

    贾玉斌

---
+++ 微球/AMapAPIKey.h
@@ -0,0 +1,16 @@
+//
+//  AMapAPIKey.h
+//  微球
+//
+//  Created by 贾玉斌 on 16/5/10.
+//  Copyright © 2016年 weiqiuwang. All rights reserved.
+//
+
+#ifndef AMapAPIKey_h
+#define AMapAPIKey_h
+
+const static NSString *APIKey = @"17a9d940d25cf01e8a8680c426f70b3a";
+
+const static NSString *TableID = @"微球";
+
+#endif /* AMapAPIKey_h */
